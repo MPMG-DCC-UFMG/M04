1e613e02b71eca56a1a7ba83410bcc05ab9323dd
leandrogripp <lggcontas@gmail.com>
2022-01-28 18:27:24 -0300

    Update modules 1 and 2

--- M04-pipeline/clean-filter-data-module/clean-filter-data.sh
+++ M04-pipeline/clean-filter-data-module/clean-filter-data.sh
@@ -9,13 +9,14 @@ NC='\033[0m' # No Color
 
 echo -e "${GREEN}Executing the Filter and Clean Data Module\n${NC}"
 
-# Setting up environment variables
-# HADOOP_HOME="/home/ufmg.m06dcc/repositories/hadoop-3.1.1"
-# HADOOP_CONF_DIR="/home/ufmg.m06dcc/repositories/hadoop-3.1.1/etc/hadoop/"
-# HDP_VERSION="3.1.5.0-152"
-# HADOOP_USER_NAME="trilhasgsi"
-# YARN_CONF_DIR="/home/ufmg.m06dcc/repositories/hadoop-3.1.1/etc/hadoop/"
-M04_PATH="/home"
+Setting up environment variables
+HADOOP_HOME="/home/ufmg.m06dcc/repositories/hadoop-3.1.1"
+HADOOP_CONF_DIR="/home/ufmg.m06dcc/repositories/hadoop-3.1.1/etc/hadoop/"
+HDP_VERSION="3.1.5.0-152"
+HADOOP_USER_NAME="trilhasgsi"
+YARN_CONF_DIR="/home/ufmg.m06dcc/repositories/hadoop-3.1.1/etc/hadoop/"
+# M04_PATH="/home"
+BASE_DIR="$(dirname "$(realpath "$0")")"
 
 CONFIG_FILE=$1
 
@@ -25,25 +26,25 @@ then
 	exit 1
 fi
 
-# # Creation of the temporary files directory
-# TEMP_INPUT="$M04_PATH/clean-filter-data-module/temp-input"
-# if [ ! -d "$TEMP_INPUT" ]; then mkdir "$TEMP_INPUT"; fi
+# Creation of the temporary files directory
+TEMP_INPUT="$BASE_DIR/temp-input"
+if [ ! -d "$TEMP_INPUT" ]; then mkdir "$TEMP_INPUT"; fi
 
-# # Creation of the raw data directory
-# RAW_INPUT="$M04_PATH/clean-filter-data-module/raw-input"
-# if [ ! -d "$RAW_INPUT" ]; then mkdir "$RAW_INPUT"; fi
+# Creation of the raw data directory
+RAW_INPUT="$BASE_DIR/raw-input"
+if [ ! -d "$RAW_INPUT" ]; then mkdir "$RAW_INPUT"; fi
 
-# # Calling the python script that fetches all of the data from the hdfs
-# # For each given file, it checks whether the fetched file is non-empty
-# # If it is empty, we keep the old one
-# python3 "$M04_PATH/clean-filter-data/data-fetching.py" $CONFIG_FILE
-# rm -rf $TEMP_INPUT
+# Calling the python script that fetches all of the data from the hdfs
+# For each given file, it checks whether the fetched file is non-empty
+# If it is empty, we keep the old one
+python3 "$BASE_DIR/data-fetching.py" $CONFIG_FILE
+rm -rf $TEMP_INPUT
 
-# echo -e "${YELLOW}Raw data fetched and brought to local\n${NC}"
+echo -e "${YELLOW}Raw data fetched and brought to local\n${NC}"
 
 # Calling the cleaning and filtering scripts. Each file is processed according to
 # what is defined in its entry on the config.json file 
 
-python3 "/home/data-processing.py" $CONFIG_FILE
+python3 "$BASE_DIR/data-processing.py" $CONFIG_FILE
 
 echo -e "${GREEN}End of execution of the Clean and Filter Data Module\n${NC}"
